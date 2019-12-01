3c662b4ef9109a61c951046ecd9d4ade66bce2b2
unknown <arjan.panesar@yahoo.co.uk>
2019-12-01 02:14:36 +0000

    tables added with methods

---
+++ src/main/QuestionTable.java
@@ -0,0 +1,240 @@
+package main;
+import java.sql.*;
+import java.util.*;
+public class QuestionTable {
+
+    public static void main (String args[]) throws SQLException {
+
+        QuestionTable et = new QuestionTable();
+        //et.CreateQuestionTable();
+        //et.Insert(12345678, 2018);
+    }
+
+    public static void CreateQuestionTable() throws SQLException {
+
+        Connection con = null; // a Connection object
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team044", "team044", "f1e121fa");
+            //=========================================================================================================
+
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                String jtable = "CREATE TABLE Question " + //Creating the table "UserTable"
+                        "(QuestionID               INT     NOT NULL    AUTO_INCREMENT, "+ //Creating the different fields
+                        "ReviewID                  INT     NOT NULL, "+
+                        "QuestionText              TEXT    NOT NULL, " +
+                        "PRIMARY KEY (QuestionID), " +
+                        "FOREIGN KEY (ReviewID) REFERENCES Review(ReviewID))";
+
+                stmt.executeUpdate(jtable);
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                    stmt.close();
+            }
+
+
+            //=========================================================================================================
+        }
+        catch (Exception e) {
+            //e.printStackTrace();
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+
+        }
+        finally {
+            if (con != null) con.close();
+        }
+
+    }
+
+    public static void Insert(int reviewid, String question ) throws SQLException {
+        Connection con = null; // connection to a database
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team044", "team044", "f1e121fa");
+            // use the open connection
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+
+                String journal = "INSERT INTO Question ( ReviewID, QuestionText) VALUES (" + reviewid +",'" + question + "')";
+                System.out.println(journal);
+                stmt.executeUpdate(journal);
+
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                    stmt.close();
+            }
+
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
+    }
+
+    public static void Delete(int id) throws SQLException {
+        Connection con = null; // connection to a database
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team044", "team044", "f1e121fa");
+            // use the open connection
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                String journal = "DELETE FROM Question WHERE QuesionID = " + id;
+                //System.out.println(journal);
+                stmt.executeUpdate(journal);
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                    stmt.close();
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
+    }
+
+
+
+
+    public static void UpdateReviewID(int questionid, int reviewid) throws SQLException {
+        Connection con = null; // connection to a database
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team044", "team044", "f1e121fa");
+            // use the open connection
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+
+                String journal = "UPDATE Question SET ReviewID = " + reviewid + " WHERE QuestionID= " + questionid;
+                //System.out.println(journal);
+                stmt.executeUpdate(journal);
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                    stmt.close();
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
+    }
+
+    public static void UpdateQuestionText(int id, String question) throws SQLException {
+        Connection con = null; // connection to a database
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team044", "team044", "f1e121fa");
+            // use the open connection
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+
+                String journal = "UPDATE Question SET QuestionText = '" + question + "' WHERE QuestionID= " + id;
+                //System.out.println(journal);
+                stmt.executeUpdate(journal);
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                    stmt.close();
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
+    }
+
+    public int SelectReviewID(int id) throws SQLException {
+        int fin = 0;
+        Connection con = null; // connection to a database
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team044", "team044", "f1e121fa");
+            // use the open connection
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                String query = "SELECT ReviewID FROM Question WHERE QuestionID = " + id;
+                ResultSet res = stmt.executeQuery(query);
+                while (res.next()) {
+                    fin = res.getInt("ReviewID");
+                }
+                res.close();
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                    stmt.close();
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
+        return fin;
+    }
+
+    public String SelectQuestionText(int id) throws SQLException {
+        String fin = null;
+        Connection con = null; // connection to a database
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team044", "team044", "f1e121fa");
+            // use the open connection
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                String query = "SELECT QuestionText FROM Question WHERE QuestionID = " + id;
+                ResultSet res = stmt.executeQuery(query);
+                while (res.next()) {
+                    fin = res.getString("QuestionText");
+                }
+                res.close();
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                    stmt.close();
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
+        return fin;
+    }
+}
+
