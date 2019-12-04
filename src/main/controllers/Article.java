58f126bc9de154887dfb0682097e2a0e63b09fd0
Joshua Robert <joshuarobert@pc-79-67.customer.ask4.lan>
2019-12-04 23:58:42 +0000

    completed

--- src/main/controllers/Article.java
+++ src/main/controllers/Article.java
@@ -1,4 +1,50 @@
 package main.controllers;
 
+
+import java.io.File;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
 public class Article {
+
+    private String name;
+    private String code;
+    private CheckBox checkbox;
+
+    public Article(String name,String code){
+        this.name=name;
+        this.code = code;
+        this.checkbox = new CheckBox();
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public CheckBox getCheckbox(){
+        return checkbox;
+    }
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setCode(String code){
+        this.code = code;
+    }
+
+    public void setCheckbox(CheckBox checkbox){
+        this.checkbox = checkbox;
+    }
+
+
 }
