200f5d0c0c7eb80eeeffb484e6600cdc7c0b2bd2
Yash Ghelani <yashghelani08@gmail.com>
2019-12-07 16:43:30 +0000

    updating usertable

--- src/main/tables/UserTable.java
+++ src/main/tables/UserTable.java
@@ -131,4 +131,37 @@ public class UserTable {
 
         return id;
     }
+
+    public static String SelectAffiliation(int id) throws SQLException {
+        String fin = null;
+        Connection con = null; // connection to a database
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team044", "team044", "f1e121fa");
+            // use the open connection
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                String query = "SELECT Affiliation FROM User WHERE UserID = " + id;
+                ResultSet res = stmt.executeQuery(query);
+                while (res.next()) {
+                    fin = res.getString("Affiliation");
+                }
+                res.close();
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null)
+                    stmt.close();
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
+        return fin;
+    }
 }
